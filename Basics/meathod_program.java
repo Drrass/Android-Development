a3b66c747c0ac024e012a1ff6b5eaf22c348e7b8
Himanshu Mehra <100959929+Drrass@users.noreply.github.com>
2023-01-18 19:59:12 +0530

    Create meathod_program.java

---
+++ Basics/meathod_program.java
@@ -0,0 +1,19 @@
+Meathods are used to reduse redundancy and to resue the code.
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	   
+	   int x=add();
+	    
+		System.out.println("sum of a and b is :"+x);
+	}
+
+	public static int add(){
+	     int a=5;
+	    int b=10;
+	    int sum=a+b;
+	    return sum;
+	}
+}
