c8d0ad6bf1bb75ca57b308d4f4a20cd673f26da4
Himanshu Mehra <100959929+Drrass@users.noreply.github.com>
2023-01-18 20:01:53 +0530

    Update sum of two integer value.java

--- Basics/sum of two integer value.java
+++ Basics/sum of two integer value.java
@@ -8,3 +8,24 @@ public class Main
 		System.out.println("sum of a and b is :"+sum);
 	}
 }
+
+
+Another way of working on meathods is by passing parameter .
+	
+	
+public class Main
+{
+	public static void main(String[] args) {
+	   
+	   int x=add(100,200);
+	    
+		System.out.println("sum of a and b is :"+x);
+	}
+
+	public static int add(int a,int b){
+	     
+	    int sum=a+b;
+	    return sum;
+	}
+}
+
